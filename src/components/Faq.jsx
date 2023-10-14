e0bf479d0f6e9a4683b2f3c7bd84e1a8352027ff
Ayush <84891062+Ayush945@users.noreply.github.com>
2023-10-14 12:41:13 +0545

    final change

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,8 +1,32 @@
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 
 function Faq() {
+    const [faqs, setFaqs] = useState([
+        {
+            question: "What is special about rental car deals?",
+            answer: "Rental car deals offer competitive prices and flexibility in choosing vehicles.",
+            isOpen: false,
+        },
+        {
+            question: "How do I find such low rental car prices?",
+            answer: "You can find low rental car prices by comparing rates from different providers and booking in advance.",
+            isOpen: false,
+        },
+        {
+            question: "How do I find the car rental deals?",
+            answer: "You can find car rental deals on our website by searching for available vehicles and comparing prices.",
+            isOpen: false,
+        }
+    ])
+
+    const toggleFaq = (index) => {
+        const newFaqs = [...faqs];
+        newFaqs[index].isOpen = !newFaqs[index].isOpen;
+        setFaqs(newFaqs);
+    }
+
     return (
         <>
             <div className='pt-20'>
@@ -12,21 +36,24 @@ function Faq() {
                     <h1 className='mt-5'>Frequently Asked Questions About the Car Rental Booking Process on Our Website: Answers to</h1>
                     <h1>Common Concerns and Inquiries.</h1>
                 </div>
-                <div className=''>
-                    <div className='flex flex-col shadow-xl border-2 items-center p-4 mr-56 ml-56 mb-10 mt-10'>
-                        <div className='flex shadow-sm w-full justify-between p-4'>
-                            <p>1. What is special about rental car deals? </p>
-                            <p><FontAwesomeIcon icon={faAngleDown} /></p>
+
+                <div>
+                    {faqs.map((faq, index) => (
+                        <div key={index} className='shadow-xl p-4 mr-56 ml-56 mb-10 mt-10'>
+                            <div
+                                className={`flex w-full justify-between p-4 ${faq.isOpen ? 'open-question' : ''}`}
+                                onClick={() => toggleFaq(index)}
+                            >
+                                <p className='font-semibold text-xl'>{faq.question}</p>
+                                <p><FontAwesomeIcon icon={faAngleDown} rotation={faq.isOpen ? 180 : 0} /></p>
+                            </div>
+                            {faq.isOpen && (
+                                <div className='p-2'>
+                                    {faq.answer}
+                                </div>
+                            )}
                         </div>
-                        <div className='flex shadow-sm w-full justify-between p-4'>
-                            <p>2. How do I find such low rental car prices? </p>
-                            <p><FontAwesomeIcon icon={faAngleDown} /></p>
-                        </div>
-                        <div className='flex shadow-sm w-full justify-between p-4'>
-                            <p>3. How do I find the car rental deals? </p>
-                            <p><FontAwesomeIcon icon={faAngleDown} /></p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </>
